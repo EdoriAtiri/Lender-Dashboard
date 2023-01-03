d2b8eea075d4f2ed579cd90cd5729eaa98ef3144
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-01-03 17:23:49 +0100

    Add: Creates Header and Header Styling

--- src/Partials/Header/Header.jsx
+++ src/Partials/Header/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import style from './Header.module.scss'
 import logo from '../../assets/Images/SVG/LendsqrLogo.svg'
-import searchIcon from '../../assets/Images/SVG/Icons/SearchIcon.svg'
+import searchIcon from '../../assets/Images/SVG/SearchIcon.svg'
+import Bell from '../../assets/Images/SVG/Bell.svg'
+import Dropdown from '../../assets/Images/SVG/Dropdown.svg'
+import user from '../../assets/Images/PNG/User.png'
 
 function Header() {
   return (
@@ -12,6 +15,18 @@ function Header() {
         <input type="text" placeholder="Search for anything" />
         <button type="button">
           <img src={searchIcon} alt="Search Icon" />
+        </button>
+      </div>
+
+      <div className={style.header__end}>
+        <a href="">Docs</a>
+        <img className={style.bell} src={Bell} alt="Bell Icon" />
+        <figure>
+          <img src={user} alt="user" />
+        </figure>
+
+        <button type="button">
+          <span>Adedeji </span> <img src={Dropdown} alt="dropdown menu" />
         </button>
       </div>
     </header>
