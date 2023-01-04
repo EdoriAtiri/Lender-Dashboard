94655d24c6a42e971ba691874501a8bf121f7c21
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-01-04 16:51:34 +0100

    Add: add dashboard link icons into assets

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -7,6 +7,7 @@ function Dashboard() {
   return (
     <div className={style.container}>
       <Header />
+      <SideBar />
     </div>
   )
 }
--- src/Partials/SideBar/SideBar.jsx
+++ src/Partials/SideBar/SideBar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import style from './SideBar.module.scss'
+import Briefcase from '../../assets/Images/SVG/Sidebar/Briefcase.svg'
+import ChevronDown from '../../assets/Images/SVG/Sidebar/ChevronDown.svg'
+import Dashboard from '../../assets/Images/SVG/Sidebar/Dashboard.svg'
 
 function SideBar() {
-  return <div>SideBar</div>
+  return (
+    <div className={style.container}>
+      <button className={style.switch_organization}>
+        <img src={Briefcase} alt="briefcase" aria-hidden="true" />
+        <span className={style.switch}>Switch Organization</span>
+        <img src={ChevronDown} alt="dropdown" aria-hidden="true" />
+      </button>
+
+      <Link className={style.link}>
+        <img src={Dashboard} alt="Dashboard Icon" />
+        <span>Dashboard</span>
+      </Link>
+    </div>
+  )
 }
 
 export default SideBar
